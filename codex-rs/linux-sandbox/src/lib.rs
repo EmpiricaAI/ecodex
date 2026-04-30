Cloudymap1e/codex

Split Linux metadata runtime modules

codex-rs/linux-sandbox/src/lib.rs
@@ -12,6 +12,12 @@ mod launcher;
 #[cfg(target_os = "linux")]
 mod linux_run_main;
 #[cfg(target_os = "linux")]
+mod metadata_paths;
+#[cfg(target_os = "linux")]
+mod protected_metadata_cleanup;
+#[cfg(target_os = "linux")]
+mod protected_metadata_runtime;
+#[cfg(target_os = "linux")]
 mod proxy_routing;
 #[cfg(target_os = "linux")]
 mod vendored_bwrap;

EmpiricaAI/ecodex

codex-rs/linux-sandbox/src/lib.rs
@@ -6,6 +6,14 @@
 #[cfg(target_os = "linux")]
 mod bwrap;
 #[cfg(target_os = "linux")]
+mod file_system_protected_metadata;
+#[cfg(target_os = "linux")]
+mod file_system_protected_metadata_cleanup;
+#[cfg(target_os = "linux")]
+mod file_system_protected_metadata_monitor;
+#[cfg(target_os = "linux")]
+mod file_system_protected_metadata_runtime;
+#[cfg(target_os = "linux")]
 mod landlock;
 #[cfg(target_os = "linux")]
 mod launcher;